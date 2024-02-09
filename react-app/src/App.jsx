c8029dcf4ac5eebb446e67a9961f631303dd1160
Анастасия Новикова <ederdart@gmail.com>
2024-02-09 14:57:18 +0300

    Пройдены Компоненты до домашнего задания

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,12 +1,34 @@
 import './App.css';
 import Button from './components/Button/Button.jsx';
 import JournalItem from './components/JournalItem/JournalItem.jsx';
+import CardButton from "./components/CardButton/CardButton.jsx";
 
 function App() {
+	const data = [{
+		title: 'Подготовка к обновлению курсов',
+		text: 'Сегодня провёл весь день за',
+		date: new Date()
+	},
+	{
+		title: 'Поход в годы',
+		text: 'Горные походы открывают удивительные природные ландшафты',
+		date: new Date()
+	}];
 	return (
 		<>
+			<CardButton>
+				<JournalItem
+					title={data[0].title}
+					text={data[0].text}
+					date={data[0].date}
+				/>
+			</CardButton>
 
-			<JournalItem/>
+			<JournalItem
+				title={data[1].title}
+				text={data[1].text}
+				date={data[1].date}
+			/>
 			<Button/>
 		</>
 	);
--- react-app/src/components/JournalItem/JournalItem.jsx
+++ react-app/src/components/JournalItem/JournalItem.jsx
@@ -1,18 +1,16 @@
 import './JournalItem.css';
 
-function JournalItem() {
-	const title = 'Подготовка к обновлению курсов';
-	const date = new Date();
-	const text = 'Горные походы открывают удивительные природные ландшафты'
+function JournalItem({title, text, date}) {
+	const dateFormatted = new Intl.DateTimeFormat('ru-RU').format(date);
 
 	return (
-		<div className="journal-item">
+		<>
 			<h2 className="journal-item__header">{title}</h2>
 			<div className="journal-item__body">
-				<div className="journal-item__date">{date.toDateString()}</div>
+				<div className="journal-item__date">{dateFormatted}</div>
 				<div className="journal-item__text">{text}</div>
 			</div>
-		</div>
+		</>
 	);
 }
 
